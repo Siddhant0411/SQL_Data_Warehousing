1bd8613fa82e165f5a0a732a8272c85a681e66d6
Siddhant0411 <124017414+Siddhant0411@users.noreply.github.com>
2025-04-23 08:56:18 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -22,7 +22,7 @@ CREATE TABLE silver.crm_cust_info (
     cst_marital_status  NVARCHAR(50),
     cst_gndr            NVARCHAR(50),
     cst_create_date     DATE,
-	dwh_create_date		DATETIME DEFAULT GETDATE()
+    dwh_create_date	DATETIME DEFAULT GETDATE()
 );
 GO
 
@@ -31,14 +31,15 @@ IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
 GO
 
 CREATE TABLE silver.crm_prd_info (
-    prd_id       INT,
-    prd_key      NVARCHAR(50),
-    prd_nm       NVARCHAR(50),
-    prd_cost     INT,
-    prd_line     NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt   DATETIME,
-	dwh_create_date		DATETIME DEFAULT GETDATE()
+    prd_id          INT,
+    cat_id          NVARCHAR(50),
+    prd_key         NVARCHAR(50),
+    prd_nm          NVARCHAR(50),
+    prd_cost        INT,
+    prd_line        NVARCHAR(50),
+    prd_start_dt    DATE,
+    prd_end_dt      DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
